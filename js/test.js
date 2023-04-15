cbb995ea3a978e3d9c9479baede684e408512e9f
Redirect <letterlocker@inbox.ru>
2023-04-15 08:06:42 +0300

    #

---
+++ js/test.js
@@ -0,0 +1,17 @@
+// function sayHello(name) {
+//     console.log("Hello, " + name + "!");
+// }
+
+// function greetUser(doit) {
+//     let name = 'Joh'
+//     doit(name)
+
+// }
+// console.log('wer')
+// greetUser(sayHello);
+
+function test(a, b) {
+    debugger
+    return a + b
+}
+test(2, 3)
